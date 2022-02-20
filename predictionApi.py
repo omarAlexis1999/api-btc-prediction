a69c0f19c4a47a6c1e43ae85b262dca427d6a1b5
omarAlexis1999 <omar.sanmartin@unl.edu.ec>
2022-02-20 00:58:32 -0500

    Little mistake

--- predictionApi.py
+++ predictionApi.py
@@ -8,10 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
+    "https://react-prediction-graphics.herokuapp.com"
 ]
 
 app.add_middleware(
